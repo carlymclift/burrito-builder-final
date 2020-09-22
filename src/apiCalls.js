34345ceb0c7f6a785bce9bf091d733d833c744b9
Carly Clift <carlymclift@gmail.com>
2020-09-22 09:45:54 -0600

    Update get request and add a post for new orders

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,21 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+export const getOrders = async () => {
+  const response = await fetch('http://localhost:3001/api/v1/orders')
+  const data = await response.json()
+  return data
+}
+
+export const addOrder = async (name, ingredients) => {
+  const response = await fetch(`http://localhost:3001/api/v1/orders`, {
+      "method": "POST",
+      "headers": {
+          "content-type": "application/json"
+      },
+      "body": JSON.stringify({
+        "name": name,
+        "ingredients": ingredients
+      })
+    }
+  )
+  const message = await response.json()
+  return message
 }
